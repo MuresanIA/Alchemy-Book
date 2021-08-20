ac641ce3d007e09de7d5e050d957b252830c20dd
MuresanIA <muresan.alexandru933@gmail.com>
2021-08-20 13:41:14 +0300

    save

--- alchemy/src/app/merchant-list/merchant-list.component.ts
+++ alchemy/src/app/merchant-list/merchant-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IngredientModel } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-merchant-list',
@@ -6,6 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./merchant-list.component.css']
 })
 export class MerchantListComponent implements OnInit {
+
+  ingredients: IngredientModel[] = [
+    new IngredientModel(
+      'Blue Mountain Flower',
+      1
+    ),
+    new IngredientModel(
+      'Butterfly Wing',
+      2
+    ),
+    new IngredientModel(
+      'Imp Stool',
+      1
+    )
+  ];
 
   constructor() { }
 
