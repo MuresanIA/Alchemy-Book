5869dbe52747130262a52fe96ecd113524190f07
MuresanIA <muresan.alexandru933@gmail.com>
2021-08-22 15:30:54 +0300

    adding navigation via event emitter

--- alchemy/src/app/app.component.ts
+++ alchemy/src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alchemy';
+  
+  loadedFeature = 'potion';
+
+  onNavigate(feature: string){
+    this.loadedFeature = feature;
+  }
 }
